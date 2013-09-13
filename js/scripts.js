ea6a88057291d3e773128671c29a7993bc924fb6
Danny Englander <danny.englander@jacksonriver.com>
2013-09-13 07:09:06 -0700

    Misc updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 /**
- * @file
- * Misc JQuery scripts in this file
- */
+* @file
+* Misc JQuery scripts in this file
+*/
 
 (function ($) {
 
 // Add drupal 7 code.
-  Drupal.behaviors.miscGratis = {
-    attach:function (context, settings) {
+Drupal.behaviors.miscGratis = {
+  attach:function (context, settings) {
 // End drupal calls.
 
 /* 
@@ -30,42 +30,41 @@ $('#main-menu ul li').each(function() {
 
 // Set li > a depths for better theming.
 $('#main-menu ul li a').each(function() {
-        var depth = $(this).parents('ul').length;
-        $(this).addClass('ula-depth-' + depth);
-      });
+  var depth = $(this).parents('ul').length;
+  $(this).addClass('ula-depth-' + depth);
+});
 
 // Start flexnav.
 if($().flexNav) {
   $(".flexnav").flexNav();
+
+//Make nav arrow and link uniform classes.
+$('#main-menu li.level-1').on('mouseover', function() {
+  var li$ = $(this);
+  li$.parent('ul').find('li').removeClass('hover-state');
+  li$.addClass('hover-state');
+}).on('mouseout', function() {
+  var li$ = $(this);
+  li$.removeClass('hover-state');
+  li$.parent('ul').find('li.current').addClass('hover-state');
+});
+
 }
 
-      // End mobile menu.
-
 // Prepend the post date before the h1.
-  $(".date-in-parts")
-    .prependTo(".not-front.page-node #post-content");
+$(".date-in-parts")
+.prependTo(".not-front.page-node #post-content");
 
 // Global zebra stripes and first / last.
-  $("article:visible:even").addClass("even");
-  $("article:visible:odd").addClass("odd");
-  $("#post-content article:last").addClass("last");
-  $("#post-content article:first").addClass("first");
+$("article:visible:even").addClass("even");
+$("article:visible:odd").addClass("odd");
+$("#post-content article:last").addClass("last");
+$("#post-content article:first").addClass("first");
 
 // Add a wrapper around the image field.
-  $('.field-type-image li') .each(function () {
-    $(this).wrapInner('<span class="image-block">');
-  });
-
-  // Make nav arrow and link uniform classes.
-// $('#main-menu li.level-1').on('mouseover', function() {
-//     var li$ = $(this);
-//     li$.parent('ul').find('li').removeClass('hover-state');
-//     li$.addClass('hover-state');
-// }).on('mouseout', function() {
-//     var li$ = $(this);
-//     li$.removeClass('hover-state');
-//     li$.parent('ul').find('li.current').addClass('hover-state');
-// });
+$('.field-type-image li') .each(function () {
+  $(this).wrapInner('<span class="image-block">');
+});
 
 // Add comment icons using font awesome.
 $('.comment-add').prepend('<i class="icon-fixed-width">&#xf040;</i>');
@@ -79,9 +78,9 @@ $('.is-node article .field-name-body ul li, .field-type-text-with-summary ul li,
 $('blockquote').prepend('<i class="icon-quote-left icon-4x pull-left icon-muted"></i>');
 
 // Node block - this should come last. It gets appened to the body field if it exists.
-  if ( $(".field").hasClass("field-name-body")) {
+if ( $(".field").hasClass("field-name-body")) {
   $(".region-node-block")
   .appendTo(".field-name-body");
 }
 
-  }}}) (jQuery);
+}}}) (jQuery);
