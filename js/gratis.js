0882601aec0a6ff4c464da2d017cf2ca45cf8e28
Danny Englander <highrockmedia@gmail.com>
2014-08-11 17:50:25 -0700

    d7 2.x - coder fixes

--- js/gratis.js
+++ js/gratis.js
@@ -1,15 +1,14 @@
 /**
- * Toggle show/hide links for off canvas layout.
- *
- * Misc functions for Gratis
+ * @file
+ * Misc js for gratis 2.
  */
 
 (function ($, Drupal) {
 
-  Drupal.behaviors.gratisMiscfunctions= {
+  Drupal.behaviors.gratisMiscfunctions = {
     attach: function (context) {
 
-      // functions
+    // Misc jquery.
 
     }
   };
@@ -21,8 +20,7 @@
   Drupal.behaviors.gratisOffCanvasLayout = {
     attach: function (context) {
 
-
-      $('.l-page').click(function(e) {
+      $('.l-page').click(function (e) {
         var offCanvasVisible = $('.l-page-wrapper').hasClass('off-canvas-left-is-visible') || $('.l-page-wrapper').hasClass('off-canvas-right-is-visible');
         var targetIsOfOffCanvas = $(e.target).closest('.l-off-canvas').length !== 0;
         if (offCanvasVisible && !targetIsOfOffCanvas) {
@@ -31,23 +29,21 @@
         }
       });
 
-      $('.l-off-canvas-show--left').click(function(e) {
+      $('.l-off-canvas-show--left').click(function (e) {
         $('.l-page-wrapper').removeClass('off-canvas-left-is-visible off-canvas-right-is-visible');
         $('.l-page-wrapper').addClass('off-canvas-left-is-visible');
         e.stopPropagation();
         e.preventDefault();
       });
 
-      //
-
-      $('.l-off-canvas-show--right').click(function(e) {
+      $('.l-off-canvas-show--right').click(function (e) {
         $('.l-page-wrapper').removeClass('off-canvas-left-is-visible off-canvas-right-is-visible');
         $('.l-page-wrapper').addClass('off-canvas-right-is-visible');
         e.stopPropagation();
         e.preventDefault();
       });
 
-      $('.l-off-canvas-hide').click(function(e) {
+      $('.l-off-canvas-hide').click(function (e) {
         $('.l-page-wrapper').removeClass('off-canvas-left-is-visible off-canvas-right-is-visible');
         e.stopPropagation();
         e.preventDefault();
@@ -59,17 +55,17 @@
   /**
    * Toggle expanded menu states.
    */
-  Drupal.behaviors.dcOmegaBaseExpandMenus = {
+  Drupal.behaviors.gratisExpandMenus = {
     attach: function (context) {
 
-      // Nested off canvas menu items
+// Nested off canvas menu items
       $('.menu .expanded').not('.active-trail').removeClass('expanded');
-      $('.menu li a').each(function() {
+      $('.menu li a').each(function () {
         if ($(this).parent().children('ul').length !== 0) {
           $(this).after('<a href="#" class="nested-menu-item-toggle"></a>');
         }
       });
-      $('.nested-menu-item-toggle').click(function() {
+      $('.nested-menu-item-toggle').click(function () {
         $(this).closest('li').toggleClass('expanded');
         return false;
       });
