018b3ad76898fc667146639b7b61b708689ca287
Danny Englander <highrockmedia@gmail.com>
2014-09-17 06:19:45 -0700

    d7 2.x - major refactoring / sass bundle update

--- Gemfile
+++ Gemfile
@@ -3,25 +3,25 @@ source 'https://rubygems.org'
 group :development do
 
   # Sass, Compass and extensions.
-  gem 'sass',       '~>3.3.10'       # Sass.
+  gem 'sass', '~>3.4.0.rc.1'      # Sass.
   gem 'sass-globbing', '~>1.1.1'  # Import Sass files based on globbing pattern.
-  gem 'compass'            # Framework built on Sass.
+  gem 'compass', '~>1.0.0.rc.1'           # Framework built on Sass.
   gem 'compass-validator'       # So you can `compass validate`.
   gem 'compass-normalize'       # Compass version of normalize.css.
   gem 'compass-rgbapng'         # Turns rgba() into .png's for backwards compatibility.
   gem 'susy'                    # Susy grid framework.
-  gem 'toolkit',  '~>2.5.0'          # Compass utility from the fabulous Snugug.
-  gem 'breakpoint', '~>2.4.2'            # Manages CSS media queries.
-  gem 'oily_png'                # Faster Compass sprite generation.
-  gem 'css_parser'              # Helps `compass stats` output statistics.
-  gem 'sass-media_query_combiner'
+  gem 'toolkit', '~>2.0'         # Compass utility from the fabulous Snugug.
+  gem 'breakpoint', '~>2.0'            # Manages CSS media queries.
+ # gem 'oily_png'                # Faster Compass sprite generation.
+ # gem 'css_parser'              # Helps `compass stats` output statistics.
+   gem 'sass-media_query_combiner'
 
   # Guard
   gem 'guard'                   # Guard event handler.
   gem 'guard-compass'           # Compile on sass/scss change.
   gem 'guard-shell'             # Run shell commands.
-  gem 'guard-livereload'        # Browser reload.
-  gem 'yajl-ruby'               # Faster JSON with LiveReload in the browser.
+  #gem 'guard-livereload'        # Browser reload.
+  #gem 'yajl-ruby'               # Faster JSON with LiveReload in the browser.
 
   # Dependency to prevent polling. Setup for multiple OS environments.
   # Optionally remove the lines not specific to your OS.
@@ -31,3 +31,4 @@ group :development do
   gem 'rb-fchange', :require => false                # Windows
 
 end
+
--- config.rb
+++ config.rb
@@ -1,26 +1,20 @@
-##
-## This file is only needed for Compass/Sass integration. If you are not using
-## Compass, you may safely ignore or delete this file.
-##
-## If you'd like to learn more about Sass and Compass, see the sass/README.txt
-## file for more information.
-##
+#
+# This file is only needed for Compass/Sass integration. If you are not using
+# Compass, you may safely ignore or delete this file.
+#
+# If you'd like to learn more about Sass and Compass, see the sass/README.txt
+# file for more information.
+#
 
-# Default to development if environment is not set.
-saved = environment
-if (environment.nil?)
-  environment = :development
-else
-  environment = saved
-end
-
-http_path = "/"
+# Change this to :production when ready to deploy the CSS to the live server.
+environment = :development
+#environment = :production
 
 # Location of the theme's resources.
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "images"
-generated_images_dir = images_dir + "/generated"
+css_dir         = "css"
+sass_dir        = "sass"
+extensions_dir  = "sass-extensions"
+images_dir      = "images"
 javascripts_dir = "js"
 
 # Require any additional compass plugins installed on your system.
@@ -32,34 +26,32 @@ require 'sass-globbing'
 require 'sass-media_query_combiner'
 require 'breakpoint'
 
-# To enable relative paths to assets via compass helper functions. Since Drupal
-# themes can be installed in multiple locations, we don't need to worry about
-# the absolute path to the theme from the server omega.
-relative_assets = true
-disable_warnings = true
+# Assuming this theme is in sites/*/themes/THEMENAME, you can add the partials
+# included with a module by uncommenting and modifying one of the lines below:
+#add_import_path "../../../default/modules/FOO"
+#add_import_path "../../../all/modules/FOO"
+#add_import_path "../../../../modules/FOO"
 
-# Output debugging info in development mode.
-#sass_options = (environment == :production) ? {} : {:debug_info => true}
-
-# Add the 'sass' directory itself as an import path to ease imports.
-add_import_path 'sass'
-
-# You can select your preferred output style here (:expanded, :nested, :compact
-# or :compressed).
-# output_style = (environment == :production) ? :expanded : :nested
-
-#set a pretty output style.
-output_style = :expanded
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = (environment == :development) ? :expanded : :compressed
 
 #Chrome web inspector sass debugging
 sass_options = {:sourcemap => true}
 enable_sourcemaps = true
 
-#Firesass extension
-firesass = true
+# To enable relative paths to assets via compass helper functions. Since Drupal
+# themes can be installed in multiple locations, we don't need to worry about
+# the absolute path to the theme from the server root.
+http_path = "/"
+relative_assets = true
+disable_warnings = true
 
-environment = :development
-#environment = :production
-
-# Conditionally enable line comments when in development mode.
+# Conditionally enable line comments.
+#Ideally keep this at false and use FireSass or Chrome source maps.
+# see: http://thesassway.com/intermediate/using-source-maps-with-sass
 line_comments = false
+
+
+# Removing all comments by applying a monkey patch to SASS compiler
+require "./remove-all-comments-monkey-patch"
