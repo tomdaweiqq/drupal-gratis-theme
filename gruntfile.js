cf20f31bff74c0b7e3c76bc476de6757e983261f
danny-englander <highrockmedia@gmail.com>
2015-07-18 07:46:51 -0700

    7.x - beginning refactor

--- gruntfile.js
+++ gruntfile.js
@@ -1,40 +1,34 @@
 module.exports = function (grunt) {
   require('time-grunt')(grunt);
-  grunt.loadNpmTasks('grunt-cssbeautifier');
-  grunt.loadNpmTasks('grunt-strip-css-comments');
-  grunt.loadNpmTasks('grunt-notify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-csscomb');
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
 
     // Define paths.
     paths: {
-      sass: 'source/sass',
-      devCSS: 'css',
-      prodCSS: 'source/deploy/styles',
+      sass: 'css-source',
+      devCSS: 'css'
     }, // paths
 
     uglify: {
       global: {
         files: {
-          "js/site.min.js": ['source/js/*.js']
+          "js/site.min.js": ['js-source/*.js']
         }
       }
     }, // uglify
 
-    cssbeautifier: {
-      global: {
-        files: {
-          src: ['css/*.css']
-        },
-        options: {
-          indent: '  ',
-          openbrace: 'end-of-line',
-          autosemicolon: false
-        }
+    csscomb: {
+      options: {
+        config: 'csscomb.json'
+      },
+      dynamic_mappings: {
+        expand: true,
+        cwd: 'css',
+        src: ['*.css', '!*.min.css'], // Pattern(s) to match.
+        dest: 'css'
       }
-    }, // cssbeautifier
+    },
 
     stripCssComments: {
       global: {
@@ -50,10 +44,8 @@ module.exports = function (grunt) {
         options: {
           sourceMap: true,
           sourceComments: false,
-          outputStyle: 'nested'
+          outputStyle: 'expanded',
         },
-        // I suspect this method slows down grunt
-        // @todo - test with named files.
         files: [{
           expand: true,
           cwd: '<%= paths.sass %>/',
@@ -73,42 +65,16 @@ module.exports = function (grunt) {
         files: ['templates/**/*.tpl.php', 'js/**/*.{js,json}', 'css/*.css', 'images/**/*.{png,jpg,jpeg,gif,webp,svg}']
       },
       js: {
-        files: ['source/js/*.js'],
+        files: ['js-source/*.js'],
         tasks: ["uglify"]
       },
       css: {
-        files: ["source/sass/**/*.scss"],
-        tasks: ["sass"]
-      },
-    }, // watch
-
-    csscomb: {
-      options: {
-        config: 'csscomb.json'
-      },
-      dynamic_mappings: {
-        expand: true,
-        cwd: 'css',
-        src: ['*.css', '!*.min.css'], // Pattern(s) to match.
-        dest: 'css'
+        files: ['css-source/**/*.scss'],
+        tasks: ["sass", "csscomb"]
       }
-    },
-
-    // Grunt notify
-    notify_hooks: {
-      options: {
-        enabled: true,
-        max_jshint_notifications: 5,
-        title: "Gratis LibSass",
-        success: true,
-        duration: 50
-      }
-    } // notify
+    } // watch
 
   });
   require("load-grunt-tasks")(grunt);
-  grunt.task.run('notify_hooks');
-  grunt.registerTask("default", ["sass", "watch"]);
-  // Grunt format command (run before code commit)
-  grunt.registerTask("format", ["csscomb", "stripCssComments",  "cssbeautifier"]);
+  grunt.registerTask("default", ["sass", "csscomb", "watch"]);
 };
