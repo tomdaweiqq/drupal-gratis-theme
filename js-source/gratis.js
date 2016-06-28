5ed610b4d01f2409de75c2edba30566b6f6af45c
Danny Englander <highrockmedia@gmail.com>
2016-06-27 17:25:25 -0700

    Misc fixes and cleanup

--- js-source/gratis.js
+++ js-source/gratis.js
@@ -1,6 +1,6 @@
 /**
  * @file
- * Misc js for gratis 2.
+ * Misc js for gratis, Drupal 8.x-3.x.
  */
 
 (function ($, Drupal) {
@@ -29,26 +29,47 @@
   /**
    * Toggle expanded menu states.
    */
-  Drupal.behaviors.gratisExpandMenus = {
+  Drupal.behaviors.gratisDesktopMenu = {
     attach: function (context) {
 
-      // Off-canvas, check for child elements on parent menu.
-      $('.main-menu-wrapper ul li').each(function () {
-        if ($('ul', this).length) {
-          $(this).addClass('has-child');
+      // Desktop menu, define variables.
+      var nav_level_one = $('#block-gratis-main-menu ul.menu-level--1 > li');
+      var nav_level_two = $('#block-gratis-main-menu ul.menu-level--2 > li');
+
+      // Desktop menu, add classes for active trail and hover states.
+      nav_level_one.hover(
+        function () {
+          $(this).addClass('active-menu-trail').removeClass('inactive-menu-trail');
+          $(this).children('ul.menu-level--2').addClass('hover').removeClass('hover-off');
+        },
+        function () {
+          $(this).addClass('inactive-menu-trail').removeClass('active-menu-trail');
+          $(this).children('ul.menu-level--2').removeClass('hover').addClass('hover-off');
+        }
+      );
+
+      nav_level_two.hover(
+        function () {
+          $(this).addClass('active-menu-trail').removeClass('inactive-menu-trail');
+          $(this).children('ul.menu-level--3').addClass('hover').removeClass('hover-off');
+        },
+        function () {
+          $(this).addClass('inactive-menu-trail').removeClass('active-menu-trail');
+          $(this).children('ul.menu-level--3').removeClass('hover').addClass('hover-off');
+        }
+      );
+
+      // Check for main menu child elements.
+      $(nav_level_one).each(function () {
+        if ($('> ul', this).length) {
+          $(this).addClass('has-child-secondary');
         }
       });
 
-      // Nested off canvas menu items.
-      $('.menu .has-child').not('.active-trail').removeClass('has-child');
-      $('.menu li a').each(function () {
-        if ($(this).parent().children('ul').length !== 0 && $(this).parent().children(".nested-menu-item-toggle").length == 0) {
-          $(this).after('<a href="#" class="nested-menu-item-toggle"></a>');
+      $(nav_level_two).each(function () {
+        if ($('> ul', this).length) {
+          $(this).addClass('has-child-tertiary');
         }
-      });
-      $('.nested-menu-item-toggle').click(function () {
-        $(this).closest('li').toggleClass('has-child');
-        return false;
       });
 
     }
